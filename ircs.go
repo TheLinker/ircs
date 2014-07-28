9b2f68e5a8f7c21bb2523d490077cffdd88a17cb
Axel Bayerl <axel.bayerl@gmail.com>
2014-07-27 22:32:11 -0300

    primera version

---
+++ ircs.go
@@ -0,0 +1,123 @@
+package main
+
+import (
+	"bufio"
+	"container/list"
+	"log"
+	"net"
+	"strings"
+)
+
+type Channel struct {
+	name  string
+	users *list.List
+	out   chan string
+}
+
+var Channels map[string]*Channel
+
+type User struct {
+	conn        net.Conn
+	nickname    string
+	username    string
+	realname    string
+	hostname    string
+	can_connect bool
+	out         chan string
+	inp         chan string
+}
+
+var Users *list.List
+
+func interpretar_comando(message string, c *User) {
+    var prefix, command, argv string
+
+    if len(message) == 0 { return }
+    if message[0] == ':' {
+        //estan mandando prefix
+        tmp := strings.SplitN(message, " ", 2)
+        prefix = strings.TrimLeft(tmp[0], ":")
+        message = tmp[1]
+    }
+
+    //obtenemos el comando
+    tmp := strings.SplitN(message, " ", 2)
+    command = tmp[0]
+    if len(tmp) > 1 {
+        argv = strings.Trim(tmp[1], " ")
+    }
+
+	log.Printf("%q %q %q\n", prefix, command, argv)
+
+    handler, ok := Mess_handlers[command]
+
+    if ok { handler(c, prefix, argv) }
+
+}
+
+func listenClient(c *User) {
+	r := bufio.NewReader(c.conn)
+	for {
+		line, err := r.ReadBytes('\n')
+		if err != nil {
+			log.Print(err)
+			break
+		}
+
+		message := strings.TrimRight(string(line), "\r\n")
+		log.Print("<- " + message)
+		interpretar_comando(message, c)
+	}
+}
+
+func sendtoChannel(c *Channel) {
+	for {
+		//message := <-c.out
+		user, message := <-c.out, <-c.out
+
+		for e := c.users.Front(); e != nil; e = e.Next() {
+			if user != e.Value.(*User).nickname {
+				e.Value.(*User).out <- message
+			}
+		}
+	}
+}
+
+func sendtoClient(c *User) {
+	for {
+		message := <-c.out
+		message += "\r\n"
+		log.Print("-> " +message)
+		c.conn.Write([]byte(message))
+	}
+}
+
+func main() {
+	Users = list.New()
+	Channels = make(map[string]*Channel)
+
+	// Listen on TCP port 2000 on all interfaces.
+	l, err := net.Listen("tcp", ":2000")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer l.Close()
+
+	for {
+		// Wait for a connection.
+		conn, err := l.Accept()
+		if err != nil {
+			log.Fatal(err)
+		}
+		// Handle the connection in a new goroutine.
+		// The loop then returns to accepting, so that
+		// multiple connections may be served concurrently.
+		tmp := new(User)
+		tmp.conn = conn
+		tmp.inp = make(chan string)
+		tmp.out = make(chan string)
+		Users.PushBack(tmp)
+		go sendtoClient(tmp)
+		go listenClient(tmp)
+	}
+}
---
+++ messages.go
@@ -0,0 +1,104 @@
+package main
+
+import (
+    "strings"
+    "fmt"
+    "log"
+    "net"
+    "container/list"
+)
+
+var Mess_handlers = map[string]func(c *User, prefix string, args string) {
+    "NICK": Nick_handler,
+    "USER": User_handler,
+    "PING": Ping_handler,
+    "JOIN": Join_handler,
+    "PRIVMSG": Privmsg_handler,
+}
+
+func Nick_handler(c *User, prefix string, args string) {
+	//por ahora lo aceptamos sin mas comprobacion
+	c.nickname = args
+	log.Print(c.nickname)
+	c.out <- fmt.Sprintf("NOTICE * :Welcome %s", c.nickname)
+}
+
+func User_handler(c *User, prefix string, args string) {
+    argv := strings.SplitN(args, " ", 4)
+    if len(argv) != 4 { return }
+
+	var hostname string
+
+	c.username = argv[0]
+    c.realname = strings.Trim(argv[3], " :")
+
+    log.Print(c.conn.RemoteAddr().String())
+	host, err := net.LookupAddr(c.conn.RemoteAddr().String())
+	if err != nil {
+		log.Print(err)
+		hostname = "localhost"
+	} else {
+		hostname = host[0]
+	}
+	c.hostname = hostname
+
+	c.out <- fmt.Sprintf("001 %s :Welcome to the Internet Relay Network %s!%s@%s", c.nickname, c.nickname, c.username, c.hostname)
+	c.out <- fmt.Sprintf("002 %s :Your host is MyIRCServer, running version 0.0.0.0.0.1", c.nickname)
+	c.out <- fmt.Sprintf("003 %s :This server was created 2014/07/26", c.nickname)
+	c.out <- fmt.Sprintf("004 %s :localhost 0.0.0.0.0.1 * *", c.nickname)
+}
+
+func Ping_handler(c *User, prefix string, args string) {
+    c.out <- fmt.Sprintf("PONG %s", args)
+}
+
+func Join_handler(c *User, prefix string, args string) {
+    argv := strings.Split(args, " ")
+    if len(argv) == 0 { return }
+
+	canal := argv[0]
+	channel, ok := Channels[canal]
+
+	if !ok {
+		tmp := &Channel{name: canal, out: make(chan string), users: list.New()}
+		Channels[canal] = tmp
+		channel = tmp
+
+		go sendtoChannel(channel)
+	}
+
+	channel.users.PushBack(c)
+
+	//ahora la respuesta
+	c.out <- fmt.Sprintf("JOIN %s", channel.name)
+
+	//motd
+	c.out <- fmt.Sprintf("332 %s %s :%s", c.nickname, channel.name, "Hola")
+
+	//usuarios conectados
+	var users string
+
+	for e := channel.users.Front(); e != nil; e = e.Next() {
+		users += " " + e.Value.(*User).nickname
+	}
+
+	c.out <- fmt.Sprintf("353 %s = %s :%s", c.nickname, channel.name, strings.TrimLeft(users, " "))
+	c.out <- fmt.Sprintf("366 %s %s :End of /NAMES list", c.nickname, channel.name)
+
+	channel.out <- c.nickname
+	channel.out <- fmt.Sprintf(":%s!%s@%s JOIN %s", c.nickname, c.username, c.hostname, channel.name)
+}
+
+func Privmsg_handler(c *User, prefix string, args string) {
+    argv := strings.SplitN(args, " ", 2)
+    if len(argv) != 2 { return }
+
+	canal := argv[0]
+	channel, ok := Channels[canal]
+	if !ok { return }
+
+	msg := strings.Trim(argv[1], ": ")
+
+	channel.out <- c.nickname
+	channel.out <- fmt.Sprintf(":%s!%s@%s PRIVMSG %s :%s", c.nickname, c.username, c.hostname, channel.name, msg)
+}
